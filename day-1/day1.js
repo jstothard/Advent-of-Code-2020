c074c1ca29b47b208b0769c7a36e084c67f4b15f
Jake Stothard <jake.stothard@arup.com>
2020-12-01 12:58:43 +0000

    switch to using dict

--- day-1/day1.js
+++ day-1/day1.js
@@ -1,18 +1,28 @@
-const findPairs = (arr) => {
+const findPairs = ([first, ...arr], target = 2020) => {
+  // Add first value to avoid unnecessary operation in the loop
+  const dict = { [first]: 1 };
+
   for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] + arr[j] === 2020) return [arr[i], arr[j]];
-    }
+    const val = arr[i];
+
+    const check = target - val;
+
+    if (dict[check]) return [val, check];
+    else dict[val] ? dict[val]++ : (dict[val] = 1);
   }
   return [];
 };
-const findTrio = (arr) => {
+const findTrio = ([first, ...arr], target = 2020) => {
+  // Add first value to avoid unnecessary operation in the loop
+  const dict = { [first]: 1 };
   for (let i = 0; i < arr.length; i++) {
+    const valI = arr[i];
     for (let j = i + 1; j < arr.length; j++) {
-      for (let k = j + 1; k < arr.length; k++) {
-        if (arr[i] + arr[j] + arr[k] === 2020) return [arr[i], arr[j], arr[k]];
-      }
+      const valJ = arr[j];
+      const check = target - valI - valJ;
+      if (dict[check]) return [valI, valJ, check];
     }
+    dict[valI] ? dict[valI]++ : (dict[valI] = 1);
   }
   return [];
 };
